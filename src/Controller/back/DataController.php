2554f19dc39d846cd4ba92764a67a9d57a9e35ae
Mohamed KAJEIOU <m.kajeiou123@gmail.com>
2022-03-12 15:47:03 +0100

    insert reports in database

--- src/Controller/back/DataController.php
+++ src/Controller/back/DataController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\back;
 use App\Entity\DataGraph;
+use App\Entity\Report;
 use App\Entity\User;
 use App\Form\DataType;
 use App\Form\ManagedataType;
@@ -12,6 +13,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 #[Route('/admin')]
 class DataController extends AbstractController
@@ -29,13 +31,29 @@ class DataController extends AbstractController
     }
 
     #[Route('/save-file', name: 'app_save_file')]
-    public function save_file()
+    public function save_file(ManagerRegistry $doctrine)
     {
+
+        $entityManager = $doctrine->getManager();
+
+        $client = $entityManager->getRepository(User::class)->find($_POST["user_id"]);
+
+        $report = new Report();
+        $date =  date("Ymd");
+        $report->setName("rapport_". $date ."_". $_POST["user_id"]);
+        $report->setUser($client);
+        $report->setCreatedAt("2022-08-14");
+
+        $entityManager->persist($report);
+        $entityManager->flush();
+
+        $lastId = $report->getId();
+
         move_uploaded_file(
             $_FILES['pdf']['tmp_name'],
-            $this->getParameter('report_upload').'/'."test4.pdf"
+            $this->getParameter('report_upload').'/'."rapport_$lastId.pdf"
         );
 
-        return new JsonResponse($_POST);
+        return new JsonResponse($lastId);
     }
 }
