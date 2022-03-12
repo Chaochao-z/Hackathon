2765baeb1398a975f85d54d033e35b0007d8dad4
Sulyk <sylvain.boudacher@gmail.com>
2022-03-12 15:58:05 +0100

    add redirect when loged

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,9 +12,10 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+
+        if ($this->getUser()) {
+            return $this->redirectToRoute('client_dashboard');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -23,6 +24,8 @@ class SecurityController extends AbstractController
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
+
+
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
